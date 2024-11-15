fc1b55f080a7b84ea0efcfc4d36d1c3a2787cba4
maj0rpain <fatslobwithafatknob@gmail.com>
2024-11-14 20:07:04 -0600

    fix netrc

--- musicbot/loader.py
+++ musicbot/loader.py
@@ -69,7 +69,7 @@ _downloader = YoutubeDL(
         "default_search": "auto",
         "cookiefile": config.COOKIE_PATH,
         "quiet": True,
-        "netrc": True
+        "usenetrc": True
     }
 )
 _preloading = {}
